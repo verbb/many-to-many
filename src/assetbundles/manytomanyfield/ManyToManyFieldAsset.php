e6f91fb8011d6dce30bc1a3c49e537cba118d714
Maarten de Graaf <wiejeben@gmail.com>
2017-09-20 11:01:14 +0200

    Add asset bundle

---
+++ src/assetbundles/manytomanyfield/ManyToManyFieldAsset.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Craft ManyToMany plugin for Craft CMS 3.x
+ *
+ * A Field Type plugin for Craft 3 that allows the management of relationships from both sides.
+ *
+ * @link      https://www.oberon.nl/
+ * @copyright Copyright (c) 2017 Oberon Amsterdam
+ */
+
+namespace OberonAmsterdam\ManyToMany\assetbundles\manytomanyfield;
+
+use Craft;
+use craft\web\AssetBundle;
+use craft\web\assets\cp\CpAsset;
+
+/**
+ * @author    Oberon Amsterdam
+ * @package   CraftManytomany
+ * @since     1.0.0
+ */
+class ManyToManyFieldAsset extends AssetBundle
+{
+    /**
+     * @inheritdoc
+     */
+    public function init(): void
+    {
+        $this->sourcePath = __DIR__ . '/dist';
+
+        $this->depends = [
+            CpAsset::class,
+        ];
+
+        $this->js = [
+            'hide-input.js',
+            'input.js'
+        ];
+
+        parent::init();
+    }
+}
