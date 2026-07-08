7df4926868bc1f118ffcc843856fc6414fa01ca4
David Malo <david@leeroy.ca>
2026-07-08 08:53:05 -0400

    no message

--- src/assetbundles/field/src/many-to-many.js
+++ src/assetbundles/field/src/many-to-many.js
@@ -23,28 +23,41 @@ Craft.ManyToMany.Field = Garnish.Base.extend({
         this.$elementSelect = this.$container.find('.js-mtm-element-select');
         this.elementSelect = this.$elementSelect.data('elementSelect');
 
-        this.elementSelect.onSelectElements = $.proxy(this, 'onSelectElements');
-        this.elementSelect.onRemoveElements = $.proxy(this, 'onRemoveElements');
+        // Use Garnish events so Craft's own onSelect/onRemove handlers still run
+        this.elementSelect.on('selectElements', $.proxy(this, 'onSelectElements'));
+        this.elementSelect.on('removeElements', $.proxy(this, 'onRemoveElements'));
     },
 
-    onSelectElements: function(elements) {
+    onSelectElements: function(ev) {
         const self = this;
+        const elements = ev.elements || [];
 
         $.each(elements, function(index, value) {
-            var i = self.elementsToDelete.indexOf(value.id);
+            var id = typeof value === 'object' && value !== null ? (value.id || $(value).data('id')) : value;
+            var i = self.elementsToDelete.indexOf(id);
+
+            if (i === -1 && typeof id === 'string') {
+                i = self.elementsToDelete.indexOf(parseInt(id, 10));
+            }
 
             if (i !== -1) {
                 self.elementsToDelete.splice(i, 1);
             }
         });
+
+        this.updateDeletedElements();
     },
 
     onRemoveElements: function() {
         const self = this;
 
+        // $elements is a jQuery collection of chip roots (with data-id), not a parent
+        // container — use filter(), not find(), matching Craft's ElementSelectInput.
         $.each(this.elementIds, function(index, value) {
-            if (!self.elementSelect.$elements.find('[data-id="' + value + '"]').length) {
-                self.elementsToDelete.push(value);
+            if (!self.elementSelect.$elements.filter('[data-id="' + value + '"]').length) {
+                if (self.elementsToDelete.indexOf(value) === -1) {
+                    self.elementsToDelete.push(value);
+                }
             }
         });
 
